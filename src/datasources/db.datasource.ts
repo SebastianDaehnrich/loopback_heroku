190bcd798ee8340593ffe5a7368cea9ca3a58383
Sebastian Dähnrich <sdaehnrich@ebay.com>
2022-03-26 13:15:19 +0100

    Added script

--- src/datasources/db.datasource.ts
+++ src/datasources/db.datasource.ts
@@ -8,9 +8,13 @@ import {juggler} from '@loopback/repository';
 
 const config = {
   name: 'db',
-  connector: 'memory',
-  localStorage: '',
-  file: './data/db.json',
+  connector: 'postgresql',
+  url: 'postgres://dcynwiafqmnfnt:a4d2532227977974ed2af794cc5c54be6c86b7347b95e95f8042ab06a1ea83ef@ec2-176-34-211-0.eu-west-1.compute.amazonaws.com:5432/d789sai80jjqn',
+  host: 'ec2-176-34-211-0.eu-west-1.compute.amazonaws.com',
+  port: 5432,
+  user: 'dcynwiafqmnfnt',
+  password: 'a4d2532227977974ed2af794cc5c54be6c86b7347b95e95f8042ab06a1ea83ef',
+  database: 'd789sai80jjqn',
 };
 
 // Observe application's life cycle to disconnect the datasource when
